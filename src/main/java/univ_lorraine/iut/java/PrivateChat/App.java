cdc6daa9fc1b71e07107441f2c80a38b04e596ff
Alexandre <a.blanc100@laposte.net>
2023-03-30 08:27:28 +0200

    Improve model and app

--- src/main/java/univ_lorraine/iut/java/PrivateChat/App.java
+++ src/main/java/univ_lorraine/iut/java/PrivateChat/App.java
@@ -19,6 +19,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("login"), 640, 480);
         stage.setScene(scene);
+        stage.setTitle("PrivateChat");
         stage.show();
     }
 
