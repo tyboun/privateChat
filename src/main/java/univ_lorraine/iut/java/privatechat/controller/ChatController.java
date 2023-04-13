29bbe0977fb4dff42dacbb36005e78f670c8133a
LOUIS Nicolas <nicolas.louis8@etu.univ-lorraine.fr>
2023-04-13 11:48:15 +0200

    avancée un peu

--- src/main/java/univ_lorraine/iut/java/privatechat/controller/ChatController.java
+++ src/main/java/univ_lorraine/iut/java/privatechat/controller/ChatController.java
@@ -2,13 +2,32 @@ package univ_lorraine.iut.java.privatechat.controller;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import univ_lorraine.iut.java.privatechat.App;
 
 public class ChatController {
 
+    public TextArea TextShowed;
+    //TextArea.setEditable(false);
+    public TextField messSend;
+    public Button buttonSend;
+
     @FXML
     private void logout() throws IOException {
         App.setRoot("login");
+    }
+    private void send() throws IOException {
 
+        App.setRoot("login");
+    }
+    private void receive() throws IOException {
+
+        App.setRoot("login");
+    }
+    private void ajoutContact() throws IOException {
+
+        App.setRoot("login");
     }
 }
--- src/main/java/univ_lorraine/iut/java/privatechat/controller/LoginController.java
+++ src/main/java/univ_lorraine/iut/java/privatechat/controller/LoginController.java
@@ -1,15 +1,42 @@
 package univ_lorraine.iut.java.privatechat.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import univ_lorraine.iut.java.privatechat.App;
+import univ_lorraine.iut.java.privatechat.clientserver.User;
 
 public class LoginController {
 
+    public PasswordField password;
+    public TextArea id;
+
     @FXML
     private void login() throws IOException, ClassNotFoundException {
-        App.setRoot("chat");
-        String args[] = new String[0];
-        univ_lorraine.iut.java.privatechat.clientserver.Serveur.main(args);
+        File passwordFile = new File("src/main/java/univ_lorraine/iut/java/privatechat/data/password.pwd");
+        Path path = passwordFile.toPath();
+        List<String> idMdp = Files.readAllLines(path);
+        if (!passwordFile.exists()) {
+            passwordFile.createNewFile();
+            Files.write(path, password.getText().concat("\n").getBytes());
+            Files.write(path, id.getText().concat("\n").getBytes(), StandardOpenOption.APPEND);
+            App.setRoot("chat");
+        } else if (idMdp.get(0).equals(id.getText()) && idMdp.get(1).equals(password.getText())) {
+            App.setRoot("chat");
+        } else {
+            password.setText("");
+            id.setText("");
+        }
     }
 }
+
