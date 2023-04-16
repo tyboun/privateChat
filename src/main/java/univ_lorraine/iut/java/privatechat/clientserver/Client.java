1ee3635c8fd166777c377f71e3ff4aa8bcdfd121
SCHOOR Adrien <adrien.schoor9@etu.univ-lorraine.fr>
2023-04-16 17:01:49 +0200

    thread communication

--- src/main/java/univ_lorraine/iut/java/privatechat/clientserver/Client.java
+++ src/main/java/univ_lorraine/iut/java/privatechat/clientserver/Client.java
@@ -1,5 +1,8 @@
 package univ_lorraine.iut.java.privatechat.clientserver;
 
+import univ_lorraine.iut.java.privatechat.model.Message;
+import univ_lorraine.iut.java.privatechat.model.MessageType;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -20,6 +23,13 @@ public class Client {
 		ObjectOutputStream oos = null;
 		ObjectInputStream ois = null;
 		Scanner keyboard = new Scanner(System.in);
+		String ecris2 = keyboard.nextLine();
+		Message message3 = new Message();
+		message3.setContent(ecris2);
+		message3.setType(MessageType.MESSAGE);
+		var thread = new ClientCommunication(socket);
+		thread.sendMessage(message3);
+		thread.start();
 
 		// establish socket connection to server
 		socket = new Socket(host.getHostName(), 9876);
@@ -27,11 +37,11 @@ public class Client {
 		oos = new ObjectOutputStream(socket.getOutputStream());
 		System.out.println("tu peux envoyer un message");
 
-	oos.writeObject(keyboard.nextLine());
+	oos.writeObject(thread);
 	oos.flush();
 	// read the server response message
 	ois = new ObjectInputStream(socket.getInputStream());
-	String message = (String) ois.readObject();
+	Message message = (Message) ois.readObject();
 	System.out.println("Message: " + message);
 		/*
 		oos.writeObject("Test 2");
--- src/main/java/univ_lorraine/iut/java/privatechat/clientserver/ClientCommunication.java
+++ src/main/java/univ_lorraine/iut/java/privatechat/clientserver/ClientCommunication.java
@@ -1,41 +1,54 @@
 package univ_lorraine.iut.java.privatechat.clientserver;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import univ_lorraine.iut.java.privatechat.model.Message;
+import univ_lorraine.iut.java.privatechat.model.MessageType;
+
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
-public class ClientCommunication implements Runnable {
+
+public class ClientCommunication extends Thread {
 
 	private Socket socket;
 	private ObjectInputStream ois;
 	private ObjectOutputStream oos;
+	private final BlockingQueue<Message> messageQueue = new LinkedBlockingQueue<>();
+	private ClientCommunication.MessageHandler messageHandler;
+	public void MessageThread(ClientCommunication.MessageHandler handler) {
+		messageHandler = handler;
+	}
+
+	public void mess(ClientCommunication.MessageHandler messageHandler) {
+		this.messageHandler = messageHandler;
+	}
+
+	public void sendMessage(Message message) {
+		messageQueue.add(message);
+	}
+	public interface MessageHandler {
+		void handleMessage(Message message);
+	}
 
 	public ClientCommunication(Socket socket) {
 		super();
 		this.socket = socket;
-
 	}
 
 	@Override
 	public void run() {
-		Scanner keyboard = new Scanner(System.in);
-		try {
-			this.ois = new ObjectInputStream(socket.getInputStream());
-			this.oos = new ObjectOutputStream(socket.getOutputStream());
-			
-			String message = (String) ois.readObject();
-			System.out.println("Message Received: " + message);
-
-			oos.writeObject(keyboard.nextLine());
-			
-
-		} catch (Exception e) {
-
+		while (!isInterrupted()) {
+			try {
+				Message message = messageQueue.take();
+				messageHandler.handleMessage(message);
+			} catch (InterruptedException e) {
+				interrupt();
+			}
 		}
-
 	}
+
 
 	public void close() {
 		try {
@@ -49,3 +62,5 @@ public class ClientCommunication implements Runnable {
 	}
 
 }
+
+
